e646855d3c1baa7b1a366b74085db5a22f459a44
Florian Huber <florian.huber@hs-duesseldorf.de>
2026-02-02 15:25:14 +0100

    add function to init

--- chemap/__init__.py
+++ chemap/__init__.py
@@ -1,9 +1,10 @@
 from .data_loader import DatasetLoader
-from .fingerprint_computation import FingerprintConfig, compute_fingerprints
+from .fingerprint_computation import FingerprintConfig, compute_fingerprints, mol_from_smiles
 
 
 __all__ = [
     "FingerprintConfig",
     "compute_fingerprints",
     "DatasetLoader",
+    "mol_from_smiles",
 ]
