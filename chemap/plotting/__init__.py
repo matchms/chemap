41920fbc0cca51252d4da3f5acd3b2755f7a8146
Florian Huber <florian.huber@hs-duesseldorf.de>
2026-02-11 17:24:36 +0100

    add init

---
+++ chemap/plotting/__init__.py
@@ -0,0 +1,24 @@
+from .colormap_handling import (
+    CmapPaletteConfig,
+    LabelMapConfig,
+    PaletteConfig,
+    PresentPairsConfig,
+    build_hier_label_map,
+    make_hier_palette,
+    map_classes_to_display_labels,
+    palette_from_cmap,
+    sorted_present_pairs,
+)
+
+
+__all__ = [
+    "CmapPaletteConfig",
+    "LabelMapConfig",
+    "PaletteConfig",
+    "PresentPairsConfig",
+    "build_hier_label_map",
+    "make_hier_palette",
+    "map_classes_to_display_labels",
+    "palette_from_cmap",
+    "sorted_present_pairs",
+]
