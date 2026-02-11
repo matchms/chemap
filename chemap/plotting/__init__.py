03533dc4ce005ad8d6c9ec71b36597e0c19336eb
Florian Huber <florian.huber@hs-duesseldorf.de>
2026-02-11 18:25:51 +0100

    update init

--- chemap/plotting/__init__.py
+++ chemap/plotting/__init__.py
@@ -4,10 +4,17 @@ from .colormap_handling import (
     PaletteConfig,
     PresentPairsConfig,
     build_hier_label_map,
+    build_selected_label_column,
+    build_selected_palette,
     make_hier_palette,
     map_classes_to_display_labels,
     palette_from_cmap,
     sorted_present_pairs,
+)
+from .scatter_plots import (
+    scatter_plot_all_classes,
+    scatter_plot_hierarchical_labels,
+    scatter_plot_selected_only,
 )
 
 
@@ -17,8 +24,13 @@ __all__ = [
     "PaletteConfig",
     "PresentPairsConfig",
     "build_hier_label_map",
+    "build_selected_label_column",
+    "build_selected_palette",
     "make_hier_palette",
     "map_classes_to_display_labels",
     "palette_from_cmap",
+    "scatter_plot_all_classes",
+    "scatter_plot_hierarchical_labels",
+    "scatter_plot_selected_only",
     "sorted_present_pairs",
 ]
