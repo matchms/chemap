74ffcde92c47b1cd8bfec84af83b2093775f53fa
Florian Huber <florian.huber@hs-duesseldorf.de>
2026-02-13 16:59:22 +0100

    update init

--- chemap/benchmarking/__init__.py
+++ chemap/benchmarking/__init__.py
@@ -1,7 +1,15 @@
+from .fingerprint_duplicates import (
+    load_duplicates_npz,
+    load_precomputed_duplicates_folder,
+    save_duplicates_npz,
+)
 from .utils import compute_compound_max_mass_differences, compute_duplicate_max_mass_differences
 
 
 __all__ = [
     "compute_compound_max_mass_differences",
     "compute_duplicate_max_mass_differences",
+    "load_duplicates_npz",
+    "load_precomputed_duplicates_folder",
+    "save_duplicates_npz",
 ]
