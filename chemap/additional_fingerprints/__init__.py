443430e64087bb2f019717f52f5a28b4ac427058
Florian Huber <florian.huber@hs-duesseldorf.de>
2026-02-15 22:44:55 +0100

    add to init

--- chemap/additional_fingerprints/__init__.py
+++ chemap/additional_fingerprints/__init__.py
@@ -1,6 +1,10 @@
 from .element_count_fp import ElementCountFingerprint
+from .map4 import MAP4FPGen
+from .mhfp import MHFPEncoderLite
 
 
 __all__ = [
     "ElementCountFingerprint",
+    "MAP4FPGen",
+    "MHFPEncoderLite",
 ]
