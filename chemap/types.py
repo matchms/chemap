77968816cc692639ab17debb3520d5b57d8d5a41
Florian Huber <florian.huber@hs-duesseldorf.de>
2026-02-16 15:54:13 +0100

    add types

--- chemap/types.py
+++ chemap/types.py
@@ -1,7 +1,10 @@
 from typing import Mapping, Sequence, Tuple, Union
+import numpy as np
 
 
 Bins = Sequence[Tuple[float, float]]
 Color = Tuple[float, float, float]  # RGB
 ColorA = Tuple[float, float, float, float]  # RGBA
 Palette = Mapping[str, Union[Color, ColorA]]
+UnfoldedBinary = list[np.ndarray]  # list of int64 feature IDs per molecule
+UnfoldedCount = list[tuple[np.ndarray, np.ndarray]]  # (int64 feature IDs, float32 values)
